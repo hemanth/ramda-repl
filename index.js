9e748256fe068f056c1060b5911e3ac4c24c3ace
Matheus Brasil <matheus.brasil10@gmail.com>
2016-01-17 23:52:38 -0200

    Prepare for better user greeting

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 'use strict';
 var os = require('os'),
+greetings = require('./greetings'),
 empty = '(' + os.EOL + ')',
 has = Object.prototype.hasOwnProperty;
 console.log('Welcome to Ramda REPL!\n');
 
 module.exports = function() {
-	var repl = require('repl').start({'prompt': 'λ > '});
+    var repl = require('repl').start({'prompt': 'λ > '});
 
-	require('repl.history')(repl, process.env.HOME + '/.ramda_history');
+    require('repl.history')(repl, process.env.HOME + '/.ramda_history');
 
-	var R = repl.context.R = require('ramda');
+    var R = repl.context.R = require('ramda');
 
-	for (var key in R){
-		if (has.call(R, key)) {
-			repl.context[key] = R[key];
-		}
-	}
+    for (var key in R){
+        if (has.call(R, key)) {
+            repl.context[key] = R[key];
+        }
+    }
 
-	var F = repl.context.fantasy =  require('ramda-fantasy');
+    var F = repl.context.fantasy =  require('ramda-fantasy');
 
-	R.map(function(f){repl.context[f] = F[f]},R.keys(F))
+    R.map(function(f){repl.context[f] = F[f]},R.keys(F))
 };
 

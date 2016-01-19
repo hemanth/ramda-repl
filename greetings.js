6a1021c6330e9546a0a5f8a91cdf13d7b4549397
Matheus Brasil <matheus.brasil10@gmail.com>
2016-01-18 23:28:37 -0200

    Draw ramda logo + greetings text

--- greetings.js
+++ greetings.js
@@ -3,6 +3,20 @@
 var chalk = require('chalk');
 
 var greetings = '\n' +
+    chalk.magenta(' λλλλλλλλ                                          \n') +
+    chalk.magenta("λλλλλλλλλλ                                         \n") +
+    chalk.magenta("λλ λλλλλλλλλλλ                                     \n") +
+    chalk.magenta("λλλλλλλλλλλλλλλ                                    \n") +
+    chalk.magenta("   λλλλλλλλλλλλ      λλλλ    λλλλλ   λλλλλ    λλ   \n") +
+    chalk.magenta("   λλλλ  λλλλλλ      λλ λλ   λλ      λλ λλ    λλ   \n") +
+    chalk.magenta('  λλλλ    λλλλλλ     λλλλλ   λλλλλ   λλλλλ    λλ  \n') +
+    chalk.magenta(" λλλλ      λλλλλ     λλ λλ   λλ      λλλλλ    λλ \n") +
+    chalk.magenta("λλλλ        λλλλ     λλ λλ   λλλλλ   λλ       λλλλ \n") +
+
+    chalk.gray("\n Welcome to the amazing ") +
+    chalk.magenta("Ramda ") +
+    chalk.gray("+ ") +
+    chalk.magenta("ramda-fantasy ") +
+    chalk.gray("REPL. \n");
 
 module.exports = greetings;
-

6ae87ef3147e614577b3086c67e57593d844337c
Jason Mendes <jason@honua.co>
2016-02-29 17:12:34 -1000

    Add ES2015/6

--- greetings.js
+++ greetings.js
@@ -1,21 +1,20 @@
-'use strict';
+const chalk = require('chalk');
+const greetings = '\n' +
+  chalk.magenta(' λλλλλλλλ                                                             \n') +
+  chalk.magenta("λλλλλλλλλλ                                                            \n") +
+  chalk.magenta("λλλλλλλλλλλλλλ                                                        \n") +
+  chalk.magenta("  λλλλλλλλλλλλλ         λλλλλλλ         λλλλλλλ        λλλλλλλ        λλ    \n") +
+  chalk.magenta("   λλλλ  λλλλλλλ        λλ   λλ         λλ             λλ    λ        λλ    \n") +
+  chalk.magenta("  λλλλ    λλλλλλ        λλλλλλλ         λλλλλλλ        λλλλλλλ        λλ    \n") +
+  chalk.magenta(' λλλλ      λλλλλ        λλ  λλλ         λλ             λλ             λλ    \n') +
+  chalk.magenta("λλλλ        λλλλ        λλ   λλλ        λλλλλλλ        λλ             λλλλλλ\n") +
 
-var chalk = require('chalk');
-
-var greetings = '\n' +
-    chalk.magenta(' λλλλλλλλ                                                             \n') +
-    chalk.magenta("λλλλλλλλλλ                                                            \n") +
-    chalk.magenta("λλλλλλλλλλλλλλ                                                        \n") +
-    chalk.magenta("  λλλλλλλλλλλλλ         λλλλλλλ         λλλλλλλ        λλλλλλλ        λλ    \n") +
-    chalk.magenta("   λλλλ  λλλλλλλ        λλ   λλ         λλ             λλ    λ        λλ    \n") +
-    chalk.magenta("  λλλλ    λλλλλλ        λλλλλλλ         λλλλλλλ        λλλλλλλ        λλ    \n") +
-    chalk.magenta(' λλλλ      λλλλλ        λλ  λλλ         λλ             λλ             λλ    \n') +
-    chalk.magenta("λλλλ        λλλλ        λλ   λλλ        λλλλλλλ        λλ             λλλλλλ\n") +
-
-    chalk.gray("\n Welcome to the amazing ") +
-    chalk.magenta("Ramda ") +
-    chalk.gray("+ ") +
-    chalk.magenta("ramda-fantasy ") +
-    chalk.gray("REPL. \n");
+  chalk.gray("\n Welcome to the amazing ") +
+  chalk.magenta("Ramda ") +
+  chalk.gray("+ ") +
+  chalk.magenta("ramda-fantasy ") +
+  chalk.gray("+ ") +
+  chalk.magenta("ES2015 ") +
+  chalk.gray("REPL. \n");
 
 module.exports = greetings;
